28b059c184d8075a2bbea6b18156d8a6e49ccaa3
Afrodance <zhixuan_wang@outlook.de>
2024-08-29 16:01:56 +0200

    create new grid after input

--- JS.js
+++ JS.js
@@ -4,3 +4,50 @@ for (i=0;i<16*16;i++){
     gridItem.classList.add('gridStyle');
     container.appendChild(gridItem);
 }
+
+const divs = document.querySelectorAll(".gridStyle");
+
+for (let i=0;i<divs.length;i++) {
+    divs[i].addEventListener("mouseenter", function() {
+    this.style.backgroundColor="red"});
+}
+let gridSize;
+btn=document.querySelector("#enter");
+function numberInput (){
+    let text;
+    let number = prompt("How many squares you want per side?");
+    if (number>100||number<=0||number%1!==0){
+        text="Wrong input!";
+        const p=document.querySelector("#number");
+        p.textContent=text;
+    }
+    else {
+        gridSize=number;
+        text = `Enjoy ${number}*${number} grid!`
+        const p=document.querySelector("#number");
+        p.textContent=text;
+    }
+
+    if (number) {
+        for (let i=0;i<16*16;i++){
+            divs[i].remove();
+        }
+        
+        newWidth=320/gridSize;
+        newHeight=320/gridSize;
+
+        for (i=0;i<gridSize*gridSize;i++){
+            const gridItem = document.createElement("div");
+            gridItem.style.width= `${newWidth}px`;
+            gridItem.style.height=`${newHeight}px`;
+            gridItem.style.border='1px solid black';
+            gridItem.style.boxSizing='border-box'; 
+            container.appendChild(gridItem);
+        }
+    }
+}
+btn.addEventListener("click",numberInput);
+
+
+    
+
